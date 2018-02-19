c180f9ff591df5d8c33e9504448ac597f7c805d1
Eryk Humberto Oliveira Alves <erykwho@gmail.com>
2018-02-19 09:41:48 -0300

    Add DEBUG configuration

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sanic Playground
+
+## Environment Variables
+
+Variable|Description|Value
+--------|-----------|-----
+DEBUG   |Debug mode |Anything to run in DEBUG mode, Nonexistent if not DEBUG Mode
--- run.py
+++ run.py
@@ -1,10 +1,13 @@
+import os
+
 import routes
 from app import app
 
 
 def run():
+    debug = True if os.getenv("DEBUG", False) else False
     routes.start_routes()
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=debug)
 
 
 if __name__ == "__main__":
