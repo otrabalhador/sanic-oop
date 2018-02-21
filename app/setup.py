1e51778402cddf1550bd0f97a8e7604b4b07328e
Eryk Humberto Oliveira Alves <erykwho@gmail.com>
2018-02-21 12:01:35 -0300

    Enhancement: Implement CORS

--- app/setup.py
+++ app/setup.py
@@ -1,3 +1,5 @@
+from sanic_cors import CORS
+
 import middlewares
 import routes
 from listeners import ListenerSetup, ListenerCollection
@@ -9,10 +11,14 @@ class SanicAppSetup:
         self.__app = app
 
     def setup(self):
+        self._setup_cors()
         self._setup_routes()
         self._setup_middlewares()
         self._setup_listeners()
         self._setup_tasks()
+
+    def _setup_cors(self):
+        CORS(self.__app)
 
     def _setup_routes(self):
         routes.start_routes(self.__app)
