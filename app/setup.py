b4c64bc6de47137fc504fc8f213a535d2ef6f698
Eryk Humberto Oliveira Alves <erykwho@gmail.com>
2018-02-21 12:05:16 -0300

    Enhancement: Implement /swagger endpoint for documentation of API using sanic-openapi for the price of free!

--- app/setup.py
+++ app/setup.py
@@ -1,4 +1,6 @@
+from sanic import Sanic
 from sanic_cors import CORS
+from sanic_openapi import swagger_blueprint, openapi_blueprint
 
 import middlewares
 import routes
@@ -7,7 +9,7 @@ from tasks import SanicTaskCollection, SanicTaskSetup
 
 
 class SanicAppSetup:
-    def __init__(self, app):
+    def __init__(self, app: Sanic):
         self.__app = app
 
     def setup(self):
@@ -16,6 +18,7 @@ class SanicAppSetup:
         self._setup_middlewares()
         self._setup_listeners()
         self._setup_tasks()
+        self._setup_openapi_swagger()
 
     def _setup_cors(self):
         CORS(self.__app)
@@ -33,3 +36,7 @@ class SanicAppSetup:
     def _setup_tasks(self):
         tasks = SanicTaskCollection().tasks
         SanicTaskSetup(self.__app, tasks).setup()
+
+    def _setup_openapi_swagger(self):
+        self.__app.blueprint(openapi_blueprint)
+        self.__app.blueprint(swagger_blueprint)
